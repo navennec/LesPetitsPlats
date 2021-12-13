a604c5f01effc6189f415cd0c6fa6ca0c5103420
yohan navennec <yohan.navennec@gmail.com>
2021-12-13 19:15:13 +0100

    JS ingredients innerHTML dans Div  ID

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ const dropDown = document.querySelector(".dropdown");
 // console.log(dropdownMenuAppareil, dropdownMenuAppareil, dropDown);
 
 dropDown.addEventListener("click", () => {
-  console.log("super");
+  console.log("super cool");
 });
 // //document.querySelector('taClasse').style.display = 'flex';
 // dropDown.addEventListener("click", () => {
@@ -27,10 +27,37 @@ dropDown.addEventListener("click", () => {
 
 for (let i = 0; i < recipes.length; i++) {
   // console.log(recipes[i]);
-  const ingredientsDetails = recipes[i];
-  console.log(ingredientsDetails.ingredients);
+  const ingredientsDetails = recipes[i].ingredients;
+  console.log(ingredientsDetails);
   for (let j = 0; j < ingredientsDetails.length; j++) {
     const ingredient = ingredientsDetails[j].ingredient;
-    console.log(ingredients.ingredient);
+    console.log(ingredient);
+    let elt = document.getElementById("ingredientList");
+    elt.innerHTML = ingredient;
   }
 }
+
+let displayIngredient = [];
+// injecter avec les ingrdients sous forme de tableau
+
+const ingredientDetail = recipes[i].ingredients;
+// On déclare une variable avec une chaine vide qu'on va remplir
+//en fonction des données reçues dans la boucle pour afficher les ingrédients
+let displayIngredients = "";
+// On boucle sur la partie ingrédient pour récupérer le contenu pour chaque partie de
+//chaque recette de ingredients
+for (let j = 0; j < ingredientDetail.length; j++) {
+  //On récupère les données ingredient pour chaque recette
+  const ingredient = ingredientDetail[j].ingredient;
+}
+
+// les const ne recup pas forcement les bonnnes choses de 28 à 35
+
+//
+// const elt = document.getElementById("dropdown");
+// // On récupère l'élément sur lequel on veut détecter le clic
+// elt.addEventListener("click", function () {
+//   // On écoute l'événement click
+//   elt.innerHTML = "Test click sur boutton ingrediants";
+//   // On change le contenu de notre élément pour afficher "C'est cliqué !"
+// });
